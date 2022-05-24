043f636497912b424ffb37d2f5b5c4780ee3b9bf
bresnow <bresnow@me.com>
2022-05-23 23:52:00 -0400

    GETMASTERUSER
    Signed-off-by: bresnow <bresnow@me.com>

--- app/routes/cnxt.tsx
+++ app/routes/cnxt.tsx
@@ -36,8 +36,8 @@ type LoadError = {
 };
 export let loader: LoaderFunction = async ({ params, request, context }) => {
   let { RemixGunContext } = context as LoadCtx;
-  let { auth } = RemixGunContext(Gun, request);
-  let gun = auth.getMasterUser();
+  let { auth, gun } = RemixGunContext(Gun, request);
+  let app = auth.getMasterUser();
   let data;
   try {
     data = await gun.get("pages").get("cnxt").then();
